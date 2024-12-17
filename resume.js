f009a1c16a04be456beee83af9546403fb8bb5d2
ItsJESH <jenishpaghadalin018@gmail.com>
2024-12-17 13:45:45 +0530

    Add Assignment 1,2,3

---
+++ resume.js
@@ -0,0 +1,30 @@
+document.getElementById("name").textContent = `Jenish Paghadal`;
+document.getElementById(
+  "Unen"
+).textContent = `B.Tech in Information Technology`;
+
+document.getElementById(
+  "Unname"
+).textContent = `Parul University, 2022 - Present`;
+
+document.getElementById("scen").textContent = `Senior Secondary Education`;
+
+document.getElementById(
+  "scname"
+).textContent = `JB and Karp School, 2020 - 2022`;
+
+document.getElementById("p1").textContent = `Bhasya`;
+
+document.getElementById(
+  "p1d"
+).textContent = ` Developed an interactive language learning platform that combines entertainment with education.
+ Enabled users to learn languages through real-world contexts such as movies and series clips.
+ Incorporated engaging tests and activities to assess and track progress in the learning journey.`;
+
+document.getElementById("p2").textContent = `Expose it`;
+
+document.getElementById(
+  "p2d"
+).textContent = ` Developed a social media web app that allows users to share photos and text-based posts anonymously.
+ Implemented secure login and post-sharing functionality using Flask for the backend and MySQL for data storage.
+ Designed the user interface with HTML and CSS, ensuring a clean and user-friendly experience`;
